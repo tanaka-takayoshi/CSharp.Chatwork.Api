0f29928ad7df633a249c4727f3553a2c545a9f0f
tanaka733 <tanaka733@outlook.com>
2013-11-28 15:23:57 +0900

    限定公開のAPIに一通り対応

---
+++ Chatwork.Service/ChatworkClient.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Chatwork.Service
+{
+    public class ChatWorkAuthenticationHandler : DelegatingHandler
+    {
+        readonly string apiToken;
+
+        public ChatWorkAuthenticationHandler(string apiToken)
+            : this(apiToken, new System.Net.Http.HttpClientHandler())
+        { }
+
+        public ChatWorkAuthenticationHandler(string apiToken, HttpMessageHandler innerHandler)
+            : base(innerHandler)
+        {
+            this.apiToken = apiToken;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
+        {
+            request.Headers.Add("X-ChatWorkToken", apiToken);
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+
+    public partial class ChatworkClient : IMe, IMy, IContact, IRoom
+    {
+        private static readonly string BaseUri = "https://api.chatwork.com/v1/";
+        
+        readonly HttpClient httpClient;
+
+        public long MaxResponseContentBufferSize
+        {
+            get
+            {
+                return httpClient.MaxResponseContentBufferSize;
+            }
+            set
+            {
+                httpClient.MaxResponseContentBufferSize = value;
+            }
+        }
+ 
+        public TimeSpan Timeout
+        {
+            get
+            {
+                return httpClient.Timeout;
+            }
+            set
+            {
+                httpClient.Timeout = value;
+            }
+        }
+
+        public IMe Me { get { return this; } }
+        public IMy My { get { return this; } }
+        public IContact Contract { get { return this; } }
+        public IRoom Room { get { return this; } }
+        
+        public ChatworkClient(string apiToken)
+        {
+            httpClient = new HttpClient(new ChatWorkAuthenticationHandler(apiToken));
+        }
+
+        public ChatworkClient(string apiToken, HttpMessageHandler innerHandler)
+        {
+            httpClient = new HttpClient(new ChatWorkAuthenticationHandler(apiToken, innerHandler));
+        }
+
+        private async Task<TResult> GetAsync<TResult>(string path, params KeyValuePair<string, object>[] parameters)
+        {
+            //TODO エスケープ処理
+            var requestUri = new Uri(BaseUri + path + "?" + string.Join("&", parameters.Where(p => p.Value != null).Select(p => p.Key + "=" + ConvertToString(p.Value))));
+            var request = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Get,
+                RequestUri = requestUri
+            };
+            return await SendAsync<TResult>(request);
+        }
+
+        private async Task<TResult> SendAsync<TResult>(HttpMethod method, string path, params KeyValuePair<string, object>[] parameters)
+        {
+            var request = new HttpRequestMessage()
+            {
+                Method = method,
+                Content = new FormUrlEncodedContent(parameters.Where(p => p.Value != null).Select(p => new KeyValuePair<string, string>(p.Key, ConvertToString(p.Value)))),
+                RequestUri = new Uri(BaseUri + path)
+            };
+            return await SendAsync<TResult>(request);
+        }
+
+        private async Task<TResult> SendAsync<TResult>(HttpRequestMessage request)
+        {
+            var res = await httpClient.SendAsync(request);
+            if (res.IsSuccessStatusCode)
+            {
+                return JsonConvert.DeserializeObject<TResult>(await res.Content.ReadAsStringAsync());
+            }
+            throw new Exception();
+        }
+
+        private async Task SendAsync(HttpMethod httpMethod, string path, params KeyValuePair<string, object>[] parameters)
+        {
+            var request = new HttpRequestMessage()
+            {
+                Method = httpMethod,
+                Content = new FormUrlEncodedContent(parameters.Where(p => p.Value != null).Select(p => new KeyValuePair<string, string>(p.Key, ConvertToString(p.Value)))),
+                RequestUri = new Uri(BaseUri + path)
+            };
+            var res = await httpClient.SendAsync(request);
+            if (res.IsSuccessStatusCode)
+            {
+                return;
+            }
+            throw new Exception();
+        }
+
+        private string ConvertToString(object value)
+        {
+            if (value == null)
+                throw new ArgumentNullException("value");
+            string text;
+            if (value is IEnumerable<int>)
+            {
+                text = string.Join(",", (IEnumerable<int>)value);
+            }
+            else if (value is DateTime)
+            {
+                text = ((DateTime) value).ToUnixTime().ToString();
+            }
+            else
+            {
+                text = value.ToString();
+            }
+            return text;
+        }
+    }
+
+    public interface IMe
+    {
+        Task<MeModel> GetAsync();
+    }
+
+    public partial class ChatworkClient //IMe
+    {
+        Task<MeModel> IMe.GetAsync()
+        {
+            return GetAsync<MeModel>("/me");
+        }
+    }
+
+    public interface IMy
+    {
+        Task<MyStatusModel> GetStatusAsync();
+
+        Task<IList<MyTaskModel>> GetTasksAsync(int? assigned_by_account_id = null, string status = null);
+    }
+
+    public partial class ChatworkClient //IMy
+    {
+        Task<MyStatusModel> IMy.GetStatusAsync()
+        {
+            return GetAsync<MyStatusModel>("/my/status");
+        }
+
+        Task<IList<MyTaskModel>> IMy.GetTasksAsync(int? assigned_by_account_id = null, string status = null)
+        {
+            return GetAsync<IList<MyTaskModel>>("/my/tasks"
+                , new KeyValuePair<string, object>("assigned_by_account_id", assigned_by_account_id)
+                , new KeyValuePair<string, object>("status", status));
+        }
+    }
+
+    public interface IContact
+    {
+        Task<IList<ContactModel>> GetAsync();
+    }
+
+    public partial class ChatworkClient // IContact
+    {
+        Task<IList<ContactModel>> IContact.GetAsync()
+        {
+            return GetAsync<IList<ContactModel>>("/contacts");
+        }
+    }
+
+    public interface IRoom
+    {
+        Task<IList<RoomModel>> GetAsync();
+        Task<CreatedRoomModel> CreateAsync(IEnumerable<int> members_admin_ids,
+            string name,
+            string description = null,
+            string icon_preset = null,
+            IEnumerable<int> members_member_ids = null,
+            IEnumerable<int> members_readonly_ids = null);
+        Task<RoomModel> GetRoomAsync(int room_id);
+        Task<CreatedRoomModel> UpdateRoomAsync(int room_id,
+            string name,
+            string description = null,
+            string icon_preset = null);
+        Task LeaveRoomAsync(int room_id,
+            string action_type);
+        Task<RoomMembersModel> GetRoomMembersAsync(int room_id);
+        Task<RoomMembersModel> UpdateRoomMembersAsync(int room_id,
+            IEnumerable<int> members_admin_ids,
+            IEnumerable<int> members_member_ids = null,
+            IEnumerable<int> members_readonly_ids = null);
+        Task<IList<MessageModel>> GetMessagesAsync(int room_id);
+        Task<CreatedMessageModel> SendMessgesAsync(int room_id, string body);
+        Task<MessageModel> GetMessageAsync(int room_id, int message_id);
+        Task<IList<TaskModel>> GetTasksAsync(int room_id, int? account_id = null, int? assigned_by_account_id = null, string status = null);
+        Task<CreatedTasksModel> CreateTasksAsync(int room_id,
+            string body,
+            IEnumerable<int> to_ids,
+            DateTime? limit = null);
+        Task<TaskInfoModel> GetTaskInfoAsync(int room_id, int task_id);
+        Task<IList<FileModel>> GetFilesAsync(int room_id);
+        Task<FileModel> GetFilAsync(int room_id, int file_id, bool? create_download_url = null);
+    }
+
+    public partial class ChatworkClient // IRoom
+    {
+        Task<IList<RoomModel>> IRoom.GetAsync()
+        {
+            return GetAsync<IList<RoomModel>>("/rooms");
+        }
+
+        Task<CreatedRoomModel> IRoom.CreateAsync(IEnumerable<int> members_admin_ids,
+            string name,
+            string description = null,
+            string icon_preset = null,
+            IEnumerable<int> members_member_ids = null,
+            IEnumerable<int> members_readonly_ids = null)
+        {
+            return SendAsync<CreatedRoomModel>(
+                HttpMethod.Post,
+                "/rooms",
+                new KeyValuePair<string, object>("members_admin_ids", members_admin_ids),
+                new KeyValuePair<string, object>("name", name),
+                new KeyValuePair<string, object>("icon_preset", icon_preset),
+                new KeyValuePair<string, object>("members_member_ids", members_member_ids),
+                new KeyValuePair<string, object>("members_readonly_ids", members_readonly_ids));
+        }
+
+        Task<RoomModel> IRoom.GetRoomAsync(int room_id)
+        {
+            return GetAsync<RoomModel>("/rooms/" + room_id);
+        }
+
+        Task<CreatedRoomModel> IRoom.UpdateRoomAsync(int room_id,
+            string name,
+            string description = null,
+            string icon_preset = null)
+        {
+            return SendAsync<CreatedRoomModel>(
+                HttpMethod.Put,
+                "/rooms/" + room_id,
+                new KeyValuePair<string, object>("description", description),
+                new KeyValuePair<string, object>("name", name),
+                new KeyValuePair<string, object>("icon_preset", icon_preset));
+        }
+
+        Task IRoom.LeaveRoomAsync(int room_id,
+            string action_type)
+        {
+            return SendAsync(
+                HttpMethod.Delete,
+                "/rooms/" + room_id,
+                new KeyValuePair<string, object>("action_type", action_type));
+        }
+
+        Task<RoomMembersModel> IRoom.GetRoomMembersAsync(int room_id)
+        {
+            return GetAsync<RoomMembersModel>("/rooms/" + room_id + "/members");
+        }
+
+        Task<RoomMembersModel> IRoom.UpdateRoomMembersAsync(int room_id,
+            IEnumerable<int> members_admin_ids,
+            IEnumerable<int> members_member_ids = null,
+            IEnumerable<int> members_readonly_ids = null)
+        {
+            return SendAsync<RoomMembersModel>(HttpMethod.Put,
+                "/rooms/" + room_id + "/members",
+                new KeyValuePair<string, object>("members_admin_ids", members_admin_ids),
+                new KeyValuePair<string, object>("members_member_ids", members_member_ids),
+                new KeyValuePair<string, object>("members_readonly_ids", members_readonly_ids));
+        }
+
+        Task<IList<MessageModel>> IRoom.GetMessagesAsync(int room_id)
+        {
+            throw new NotImplementedException("APIが実装されていません");
+            //return GetAsync<IEnumerable<MessageModel>>("/rooms/" + room_id + "/messages");
+        }
+
+        Task<CreatedMessageModel> IRoom.SendMessgesAsync(int room_id, string body)
+        {
+            return SendAsync<CreatedMessageModel>(HttpMethod.Post,
+                "/rooms/" + room_id + "/messages",
+                new KeyValuePair<string, object>("body", body));
+        }
+
+        Task<MessageModel> IRoom.GetMessageAsync(int room_id, int message_id)
+        {
+            return GetAsync<MessageModel>("/rooms/" + room_id + "/messages/" + message_id);
+        }
+
+        Task<IList<TaskModel>> IRoom.GetTasksAsync(int room_id, int? account_id = null, int? assigned_by_account_id = null, string status = null)
+        {
+            return GetAsync<IList<TaskModel>>("/rooms/" + room_id + "/tasks"
+                , new KeyValuePair<string, object>("account_id", account_id)
+                , new KeyValuePair<string, object>("assigned_by_account_id", assigned_by_account_id)
+                , new KeyValuePair<string, object>("status", status));
+        }
+
+        Task<CreatedTasksModel> IRoom.CreateTasksAsync(int room_id,
+            string body,
+            IEnumerable<int> to_ids,
+            DateTime? limit = null)
+        {
+            return SendAsync<CreatedTasksModel>(HttpMethod.Post
+                , "/rooms/" + room_id + "/tasks"
+                , new KeyValuePair<string, object>("body", body)
+                , new KeyValuePair<string, object>("to_ids", to_ids)
+                , new KeyValuePair<string, object>("limit", limit));
+        }
+
+        Task<TaskInfoModel> IRoom.GetTaskInfoAsync(int room_id, int task_id)
+        {
+            return GetAsync<TaskInfoModel>("/rooms/" + room_id + "/tasks/" + task_id);
+        }
+
+        Task<IList<FileModel>> IRoom.GetFilesAsync(int room_id)
+        {
+            return GetAsync<IList<FileModel>>("/rooms/" + room_id + "/files");
+        }
+
+        Task<FileModel> IRoom.GetFilAsync(int room_id, int file_id, bool? create_download_url = null)
+        {
+            return GetAsync<FileModel>("/rooms/" + room_id + "/files/" + file_id
+                , new KeyValuePair<string, object>("create_download_url", create_download_url));
+        }
+    }
+}
---
+++ Chatwork.Service/Models.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Chatwork.Service
+{
+
+    public class MeModel
+    {
+        public int account_id { get; set; }
+        public int room_id { get; set; }
+        public string name { get; set; }
+        public string chatwork_id { get; set; }
+        public int organization_id { get; set; }
+        public string organization_name { get; set; }
+        public string department { get; set; }
+        public string title { get; set; }
+        public string url { get; set; }
+        public string introduction { get; set; }
+        public string mail { get; set; }
+        public string tel_organization { get; set; }
+        public string tel_extension { get; set; }
+        public string tel_mobile { get; set; }
+        public string skype { get; set; }
+        public string facebook { get; set; }
+        public string twitter { get; set; }
+        public string avatar_image_url { get; set; }
+    }
+
+
+    public class MyStatusModel
+    {
+        public int unread_room_num { get; set; }
+        public int mention_room_num { get; set; }
+        public int mytask_room_num { get; set; }
+        public int unread_num { get; set; }
+        public int mention_num { get; set; }
+        public int mytask_num { get; set; }
+    }
+
+
+    public class MyTaskModel
+    {
+        public int task_id { get; set; }
+        public TaskRoomModel room { get; set; }
+        public AssignedByAccountModel assigned_by_account { get; set; }
+        public int message_id { get; set; }
+        public string body { get; set; }
+        [JsonProperty]
+        [JsonConverter(typeof(UnixDateTimeConverter))]
+        public DateTime limit_time { get; set; }
+        public string status { get; set; }
+    }
+
+    public class TaskRoomModel
+    {
+        public int room_id { get; set; }
+        public string name { get; set; }
+        public string icon_path { get; set; }
+    }
+
+    public class AssignedByAccountModel
+    {
+        public int account_id { get; set; }
+        public string name { get; set; }
+        public string avatar_image_url { get; set; }
+    }
+
+    public class ContactModel
+    {
+        public int account_id { get; set; }
+        public int room_id { get; set; }
+        public string name { get; set; }
+        public string chatwork_id { get; set; }
+        public int organization_id { get; set; }
+        public string organization_name { get; set; }
+        public string department { get; set; }
+        public string avatar_image_url { get; set; }
+    }
+
+    public class CreatedRoomModel
+    {
+        public int room_id { get; set; }
+    }
+
+
+    public class RoomModel
+    {
+        public int room_id { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public string role { get; set; }
+        public bool sticky { get; set; }
+        public int unread_num { get; set; }
+        public int mention_num { get; set; }
+        public int mytask_num { get; set; }
+        public int message_num { get; set; }
+        public int file_num { get; set; }
+        public int task_num { get; set; }
+        public string icon_path { get; set; }
+        [JsonProperty]
+        [JsonConverter(typeof(UnixDateTimeConverter))]
+        public DateTime last_update_time { get; set; }
+    }
+
+
+    public class RoomMembersModel
+    {
+        public int[] admin { get; set; }
+        public int[] member { get; set; }
+        public int[] _readonly { get; set; }
+    }
+
+    public class CreatedMessageModel
+    {
+        public int message_id { get; set; }
+    }
+
+    public class MessageModel
+    {
+        public int message_id { get; set; }
+        public AccountModel account { get; set; }
+        public string body { get; set; }
+        [JsonProperty]
+        [JsonConverter(typeof(UnixDateTimeConverter))]
+        public DateTime send_time { get; set; }
+        [JsonProperty]
+        [JsonConverter(typeof(UnixDateTimeConverter))]
+        public DateTime update_time { get; set; }
+    }
+
+    public class TaskModel
+    {
+        public int task_id { get; set; }
+        public TaskRoomModel room { get; set; }
+        public AccountModel account { get; set; }
+        public AssignedByAccountModel assigned_by_account { get; set; }
+        public int message_id { get; set; }
+        public string body { get; set; }
+        [JsonProperty]
+        [JsonConverter(typeof(UnixDateTimeConverter))]
+        public DateTime limit_time { get; set; }
+        public string status { get; set; }
+    }
+
+    public class AccountModel
+    {
+        public int account_id { get; set; }
+        public string name { get; set; }
+        public string avatar_image_url { get; set; }
+    }
+
+    
+    public class CreatedTasksModel
+    {
+        public int[] task_ids { get; set; }
+    }
+
+
+    public class TaskInfoModel
+    {
+        public int task_id { get; set; }
+        public AccountModel account { get; set; }
+        public AssignedByAccountModel assigned_by_account { get; set; }
+        public int message_id { get; set; }
+        public string body { get; set; }
+        [JsonProperty]
+        [JsonConverter(typeof(UnixDateTimeConverter))]
+        public DateTime limit_time { get; set; }
+        public string status { get; set; }
+    }
+
+
+    public class FileModel
+    {
+        public int file_id { get; set; }
+        public AccountModel account { get; set; }
+        public int message_id { get; set; }
+        public string filename { get; set; }
+        public int filesize { get; set; }
+        [JsonProperty]
+        [JsonConverter(typeof(UnixDateTimeConverter))]
+        public DateTime upload_time { get; set; }
+    }
+
+}
