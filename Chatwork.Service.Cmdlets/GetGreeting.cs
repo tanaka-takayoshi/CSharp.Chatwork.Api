e7ac674e47c4583ed302441b3a0bd23552a07620
tanaka_733 <tanaka733@outlook.com>
2014-03-01 04:56:46 +0900

    とりあえず

---
+++ Chatwork.Service.Cmdlets/GetGreeting.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chatwork.Service.Cmdlets
+{
+    [Cmdlet(VerbsCommon.Get, "Greeting")]
+    public class GetGreeting : Cmdlet
+    {
+        [Parameter(Position = 0, ValueFromPipeline = true)]
+        [ValidateNotNullOrEmpty]
+        public string Name { get; set; }
+
+        protected override void BeginProcessing()
+        {
+            WriteObject("Begin Hello!");
+        }
+
+        protected override void ProcessRecord()
+        {
+            WriteObject("World " + Name);
+        }
+
+        protected override void EndProcessing()
+        {
+            if (Stopping)
+            {
+                WriteObject("Stopped Hello.");
+            }
+            else
+            {
+                WriteObject("End Hello!");
+            }
+        }
+
+        protected override void StopProcessing()
+        {
+            WriteObject("Now stopping...");
+        }
+    }
+}
---
+++ Chatwork.Service.Cmdlets/GetMe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chatwork.Service.Cmdlets
+{
+    [Cmdlet(VerbsCommon.Get, "Me")]
+    public class GetMe : Cmdlet
+    {
+        protected override void EndProcessing()
+        {
+            WriteObject("hogehoge!");
+        }
+    }
+}
